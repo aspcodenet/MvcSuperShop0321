1001937550fb95607f65130ba36b642bf8c0eab9
aspcodenet <stefan.holmberg@systementor.se>
2023-03-21 12:13:10 +0100

    a

---
+++ MvcSuperShop.Integration.Tests/Services/CategoryServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using MvcSuperShop.Data;
+using MvcSuperShop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcSuperShop.Integration.Tests.Services
+{
+    [TestClass]
+    public class CategoryServiceTests
+    {
+        private CategoryService sut;
+        private ApplicationDbContext context;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
+            context = new ApplicationDbContext(contextOptions);
+            context.Database.EnsureCreated();
+
+
+            sut = new CategoryService(context);
+        }
+
+        [TestMethod]
+        public void Get_trending_categories_should_return_only_so_many()
+        {
+            int antal = 3;
+            for(int i = 0; i < 8; i++)
+            {
+                context.Categories.Add(new Category { Name = "Cat" + i, Icon="" });
+            }
+            context.SaveChanges();
+
+            var result = sut.GetTrendingCategories(antal).Count();
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ MvcSuperShop.Integration.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using MvcSuperShop.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcSuperShop.Integration.Tests.Services
+{
+    [TestClass]
+    public class UserServiceTests
+    {
+        private readonly UserService sut;
+        public UserServiceTests()
+        {
+            sut = new UserService();
+        }
+
+        [TestMethod]
+        public void Calling_api_should_map_properties()
+        {
+            var result = sut.Generate();
+
+            Assert.IsNotNull(result.Country);
+            Assert.IsNotNull(result.City);
+            Assert.IsNotNull(result.FirstName);
+            Assert.IsNotNull(result.LastName);
+            Assert.IsNotNull(result.Username);
+        }
+    }
+}
