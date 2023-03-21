97be8d0df0e53056c88471cd4a31eacf6266f7d6
aspcodenet <stefan.holmberg@systementor.se>
2023-03-21 10:38:08 +0100

    Add project files.

---
+++ MvcSuperShop/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿namespace MvcSuperShop.Services
+{
+
+
+    public class FakePerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public string Username { get; set; }
+    }
+
+    public class UserService
+    {
+        public FakePerson Generate()
+        {
+            using (var client = new HttpClient())
+            {
+                var json = client.GetStringAsync("https://randomuser.me/api/").Result;
+                dynamic r = JsonConvert.DeserializeObject<dynamic>(json);
+
+                var result = new FakePerson();
+                result.City = r.results[0].location.city;
+                result.Country = r.results[0].location.country;
+                result.FirstName = r.results[0].name.first;
+                result.LastName = r.results[0].name.last;
+                result.Username = r.results[0].login.username;
+                return result;
+            }
+        }
+    }
+}
