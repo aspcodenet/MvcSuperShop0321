33d195934b05b415f0a34a1f0fafe4b0ae2e84b7
aspcodenet <stefan.holmberg@systementor.se>
2023-03-21 12:55:37 +0100

    a

--- MvcSuperShop.Integration.Tests/Services/CategoryServiceTests.cs
+++ MvcSuperShop.Integration.Tests/Services/CategoryServiceTests.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoFixture;
 
 namespace MvcSuperShop.Integration.Tests.Services
 {
@@ -15,6 +16,7 @@ namespace MvcSuperShop.Integration.Tests.Services
     {
         private CategoryService sut;
         private ApplicationDbContext context;
+        private Fixture fixture = new Fixture();
 
         [TestInitialize]
         public void Initialize()
@@ -34,11 +36,16 @@ namespace MvcSuperShop.Integration.Tests.Services
         [TestMethod]
         public void Get_trending_categories_should_return_only_so_many()
         {
+            //var test = fixture.Create<Agreement>();
+            //test.AgreementRows[0].CategoryMatch = "van";
+            //test.AgreementRows[0].PercentageDiscount = 12;
+
             int antal = 3;
-            for(int i = 0; i < 8; i++)
-            {
-                context.Categories.Add(new Category { Name = "Cat" + i, Icon="" });
-            }
+            context.Categories.AddRange( fixture.CreateMany<Category>(8) );
+            //for(int i = 0; i < 8; i++)
+            //{
+            //    context.Categories.Add(fixture.Create<Category>());
+            //}
             context.SaveChanges();
 
             var result = sut.GetTrendingCategories(antal).Count();
